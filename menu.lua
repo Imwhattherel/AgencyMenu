4bbc6073473544aa9bb3c419860f4798a28b769d
WhatTheReL <79932956+Imwhattherel@users.noreply.github.com>
2024-09-30 18:32:01 -0400

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -1,122 +1,65 @@
 _menuPool = NativeUI.CreatePool()
-mainMenu = NativeUI.CreateMenu("Agency Menu", "~h~Set your radio agency", 720,100)    --- Title, description, placement of menu (center by default)
+mainMenu = NativeUI.CreateMenu("Agency Menu", "~h~Set your radio agency", 720, 100)
 _menuPool:Add(mainMenu)
-_menuPool:MouseControlsEnabled (false);
-_menuPool:MouseEdgeEnabled (false);
-_menuPool:ControlDisablingEnabled(false);
 
--- Used in "FirstMenu"
-bool = false
+-- Disable unnecessary controls globally for the pool
+_menuPool:MouseControlsEnabled(false)
+_menuPool:MouseEdgeEnabled(false)
+_menuPool:ControlDisablingEnabled(false)
 
--- this menu is a checkbox item
-function FirstItem(menu)
-  local submenu = _menuPool:AddSubMenu(menu, "~b~~h~LEO", "", 720,100)  --- Title Of the main button
+-- Helper function to set up agency items
+function addAgencyItem(submenuTitle, agencyName, agencyColor)
+    local submenu = _menuPool:AddSubMenu(mainMenu, agencyColor .. "~h~" .. submenuTitle, "", 720, 100)
 
-_menuPool:MouseControlsEnabled (false);
-_menuPool:MouseEdgeEnabled (false);
-_menuPool:ControlDisablingEnabled(false);
+    local rdioItem = NativeUI.CreateItem("Set Agency", "Set agency to " .. agencyColor .. agencyName)
+    rdioItem.Activated = function(sender, item)
+        if item == rdioItem then
+            TriggerEvent("radioExternal:setCodeplug", agencyName:lower())
+            notify("Agency set to " .. agencyColor .. agencyName)
+        end
+    end
 
-  local rdioItem = NativeUI.CreateItem("Set Agency", "Set agency to ~b~leo") --- button that sets the codeplug
-  rdioItem.Activated = function(sender, item)
-       if item == rdioItem then
-        TriggerEvent("radioExternal:setCodeplug","leo")           --- codeplug function 
-           notify("Agency set to ~b~Leo")                          --- Post click notification 
-       end
-  end 
-  submenu:AddItem(rdioItem)
-
+    submenu:AddItem(rdioItem)
 end
 
+-- Add LEO, Fire, and DOT items
+addAgencyItem("LEO", "LEO", "~b~")
+addAgencyItem("Fire", "Fire", "~r~")
+addAgencyItem("DOT", "DOT", "~o~")
 
-function SecondItem(menu) 
-  local submenu = _menuPool:AddSubMenu(menu, "~r~~h~Fire","", 720,100) 
+-- Credits item
+function addCreditsItem()
+    local submenu = _menuPool:AddSubMenu(mainMenu, "~h~Credits", "WhatTheReL", 720, 100)
 
-_menuPool:MouseControlsEnabled (false);
-_menuPool:MouseEdgeEnabled (false);
-_menuPool:ControlDisablingEnabled(false);
+    local contactItem = NativeUI.CreateItem("Contact", "Contact Me via discord @ whattherel", 720, 100)
+    contactItem.Activated = function(sender, item)
+        -- Action when the "Contact" item is clicked, add functionality if needed
+    end
 
-  local rdioItem = NativeUI.CreateItem("Set Agency", "Set agency to ~r~Fire")
-  rdioItem.Activated = function(sender, item)
-       if item == rdioItem then
-        TriggerEvent("radioExternal:setCodeplug","fire")
-           notify("Agency set to ~r~fire")
-       end
-  end
-  
-  submenu:AddItem(rdioItem)
-
+    submenu:AddItem(contactItem)
 end
 
+-- Add credits item
+addCreditsItem()
 
-
-function ThirdItem(menu)
-  local submenu = _menuPool:AddSubMenu(menu, "~h~~o~DOT", "",720,100)  
-
-_menuPool:MouseControlsEnabled (false);
-_menuPool:MouseEdgeEnabled (false);
-_menuPool:ControlDisablingEnabled(false);
-
-  local rdioItem = NativeUI.CreateItem("Set Agency", "Set agency to ~o~DOT")
-  rdioItem.Activated = function(sender, item)
-       if item == rdioItem then
-        TriggerEvent("radioExternal:setCodeplug","dot")
-           notify("Agency set to ~o~DOT")
-       end
-  end
-submenu:AddItem(rdioItem)
-
-end
-
-function CreditItem(menu) 
-  local submenu = _menuPool:AddSubMenu(menu, "~h~Credits","WhatTheReL", 720,100)     ----- PLEASE DO NOT REMOVE :)
-
-_menuPool:MouseControlsEnabled (false);
-_menuPool:MouseEdgeEnabled (false);
-_menuPool:ControlDisablingEnabled(false);
-
-local rdioItem = NativeUI.CreateItem("Contact","Contact Me via discord @ whattherel", 720,100) 
-  rdioItem.Activated = function(sender, item)
-       if item == rdioItem then
-    
-       end
-  end
-
- 
-submenu:AddItem(rdioItem)
-
-end
- 
-
-
-
-
-FirstItem(mainMenu)
-SecondItem(mainMenu)
-ThirdItem(mainMenu)
-CreditItem(mainMenu)
+-- Refresh menu index to show changes
 _menuPool:RefreshIndex()
 
+-- Citizen thread to handle menu visibility toggle (F9 key)
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
         _menuPool:ProcessMenus()
-        --[[ The "f9" button will activate the menu https://docs.fivem.net/docs/game-references/controls/ ]]
-        if IsControlJustPressed(1, 56) then 
+
+        if IsControlJustPressed(1, 56) then -- F9 key
             mainMenu:Visible(not mainMenu:Visible())
         end
     end
 end)
 
-
-function notify(text)                           --- DO NOT TOUCH
+-- Notification function
+function notify(text)
     SetNotificationTextEntry("STRING")
     AddTextComponentString(text)
     DrawNotification(true, true)
 end
-
-
-
-
-
-
-
